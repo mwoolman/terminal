5d5eee83cac25f612df9dd49b642bc9becb2a56e
Matthew Woolman <mwoolman@Leodora.(none)>
2011-02-20 18:20:21 -0500

    refactored code in utils to be less repetitious

--- utils.js
+++ utils.js
@@ -14,99 +14,73 @@ function createColorWidget(){
 
     var currEl;
     var colors = term.getColors();
-
-	currEl  = document.createElement('td');
-	currEl.innerHTML = "Bright Colors";
-	currEl.className = "label";
-	br.appendChild(currEl);
+	//label the bright colors cell
+	br.appendChild(createCell("Bright Colors", undefined, "label"));
 	//create color elements
     for( idx in colors.bright ){
-		currEl  = document.createElement('td');
-		currEl.style.background = colors.bright[idx];
+		currEl  = createCell( undefined, colors.bright[idx]);
 		//save reference to the element for later use
 		brightRow.push(currEl);
 		br.appendChild(currEl);
     }
 
     br = br.nextElementSibling;
-	currEl  = document.createElement('td');
-	currEl.className = "spacer";
-	br.appendChild(currEl);
+	//add spacer
+	br.appendChild(createCell( undefined, undefined, "spacer"));
 
     for( idx in colors.bright ){
-		currEl  = document.createElement('td');
-		var txt = document.createElement('input');
-		txt.type = 'text';
-		txt.value = colors.bright[idx];
+		currEl  = createCell();
+		var txt = createInputField(colors.bright[idx]);
 		currEl.appendChild(txt);
 		setBrightRow.push(txt);
 		br.appendChild(currEl);
     }
+
     br = br.nextElementSibling;
     // dark colors
-	currEl  = document.createElement('td');
-	currEl.innerHTML = "Normal Colors";
-	currEl.className = "label";
-	br.appendChild(currEl);
+	br.appendChild(createCell("Normal Colors", undefined, "label"));
 
     for( idx in colors.dark ){
-		currEl  = document.createElement('td');
-		currEl.style.background = colors.dark[idx];
+		currEl  = createCell( undefined, colors.dark[idx]);
 		darkRow.push(currEl);
 		br.appendChild(currEl);
     }
+
     br = br.nextElementSibling;
-	currEl  = document.createElement('td');
-	currEl.className = "spacer";
-	br.appendChild(currEl);
+	br.appendChild(createCell(undefined, undefined, "spacer"));
+
     for( idx in colors.dark ){
-		currEl  = document.createElement('td');
-		var txt = document.createElement('input');
-		txt.type = 'text';
-		txt.value = colors.dark[idx];
+		currEl  = createCell();
+		var txt = createInputField(colors.bright[idx]);
+
 		currEl.appendChild(txt);
 		setDarkRow.push(txt);
 		br.appendChild(currEl);
     }
 	//background
 	br = br.nextElementSibling;
-	currEl  = document.createElement('td');
-	currEl.innerHTML = "background";
-	currEl.className = "label";
-	br.appendChild(currEl);
-	currEl = document.createElement('td');
-	currEl.style.background = term.background;
+	br.appendChild(createCell("background", undefined, 'label'));
+	//create the color cell
+	currEl = createCell(undefined, term.background, undefined);
 	bgDefault.push(currEl);
 	br.appendChild(currEl);
 	//text color
-	currEl  = document.createElement('td');
-	currEl.innerHTML = "Text Color";
-	currEl.className = "label";
-	br.appendChild(currEl);
-	currEl = document.createElement('td');
-	currEl.style.background = term.getTextDefault();
+	br.appendChild(createCell('Text Color', undefined, 'label'));
+	currEl = createCell(undefined, term.getTextDefault(), undefined); 
 	txtDefault.push(currEl);
 	br.appendChild(currEl);
 	//color selector
 	br = br.nextElementSibling;
-	currEl  = document.createElement('td');
-	currEl.className = "spacer";
-	br.appendChild(currEl);
-	currEl = document.createElement('td');
-	var txt = document.createElement('input');
-	txt.type = 'text';
-	txt.value = term.background;
+	br.appendChild(createCell(undefined, undefined, 'spacer'));
+	currEl = createCell();
+	var txt = createInputField(term.background);
 	currEl.appendChild(txt);
 	bgDefault.push(txt);
 	br.appendChild(currEl);
 	//text color selector
-	currEl  = document.createElement('td');
-	currEl.className = "spacer";
-	br.appendChild(currEl);
-	currEl = document.createElement('td');
-	var txt = document.createElement('input');
-	txt.type = 'text';
-	txt.value = term.getTextDefault();
+	br.appendChild(createCell(undefined, undefined, 'spacer'));
+	currEl = createCell();
+	var txt = createInputField(term.getTextDefault());
 	currEl.appendChild(txt);
 	txtDefault.push(txt);
 	br.appendChild(currEl);
@@ -114,6 +88,23 @@ function createColorWidget(){
 }
 
 
+function createCell( text, bg, className ){
+	var el = document.createElement('td');
+	if( text == undefined ){
+		text = "";
+	}
+	el.innerHTML = text;
+	el.style.background = bg;
+	el.className = className;
+	return el;
+}
+
+function createInputField(text ){
+	var el = document.createElement('input');
+	el.type = 'text';
+	el.value = text;
+	return el;
+}
 
 function showColorSelector(){
     document.getElementById('colors').style.display = 'block';
